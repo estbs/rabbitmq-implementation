a54d1a10ad55df88c2b2ec14174defb10a727ad6
Esteban BS <estbs93@gmail.com>
2020-08-25 13:56:23 -0500

    Fix listener params

--- src/main/java/com/ebs/springboot/messages/MessagesListener.java
+++ src/main/java/com/ebs/springboot/messages/MessagesListener.java
@@ -8,7 +8,6 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Payload;
 
 public interface MessagesListener {
 
@@ -16,15 +15,15 @@ public interface MessagesListener {
     void getCreatedMessage(Message message, @Header(AmqpHeaders.CHANNEL) Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) Long deliveryTag) throws IOException;
 
     @RabbitListener(admin = "rabbitAdmin", containerFactory = "rabbitListenerContainerFactory", queues = "ebs.messages.created.1")
-    void getCreatedMessage1(@Payload String payload, @Header(AmqpHeaders.CHANNEL) Channel channel,
+    void getCreatedMessage1(Message message, @Header(AmqpHeaders.CHANNEL) Channel channel,
             @Header(AmqpHeaders.DELIVERY_TAG) Long deliveryTag) throws IOException;
 
     @RabbitListener(admin = "rabbitAdmin", containerFactory = "rabbitListenerContainerFactory", queues = "ebs.messages.created.2")
-    void getCreatedMessage2(@Payload String payload, @Header(AmqpHeaders.CHANNEL) Channel channel,
+    void getCreatedMessage2(Message message, @Header(AmqpHeaders.CHANNEL) Channel channel,
             @Header(AmqpHeaders.DELIVERY_TAG) Long deliveryTag) throws IOException;
 
     @RabbitListener(admin = "rabbitAdmin", containerFactory = "rabbitListenerContainerFactory", queues = "ebs.messages.created.3")
-    void getCreatedMessage3(@Payload String payload, @Header(AmqpHeaders.CHANNEL) Channel channel,
+    void getCreatedMessage3(Message message, @Header(AmqpHeaders.CHANNEL) Channel channel,
             @Header(AmqpHeaders.DELIVERY_TAG) Long deliveryTag) throws IOException;
 
     @RabbitListener(admin = "rabbitAdmin", containerFactory = "rabbitListenerContainerFactory", queues = "ebs.messages.created.dlq")
