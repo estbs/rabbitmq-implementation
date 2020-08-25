376aa16080fc6497fa75fd723abc5e8eaae6da03
Esteban BS <estbs93@gmail.com>
2020-08-24 21:39:42 -0500

    Add process to reject a message without requeueing and other process to requeue

--- src/main/java/com/ebs/springboot/messages/MessagesListenerImpl.java
+++ src/main/java/com/ebs/springboot/messages/MessagesListenerImpl.java
@@ -5,15 +5,24 @@ import com.rabbitmq.client.Channel;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.amqp.support.AmqpHeaders;
 
+@Slf4j
 @Component
 public class MessagesListenerImpl implements MessagesListener {
 
     @Override
-    public void getCreatedMessage(@Payload String payload, @Header(AmqpHeaders.CHANNEL) Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) Long deliveryTag) {
-        // TODO Auto-generated method stub
-
+    public void getCreatedMessage(@Payload String payload, @Header(AmqpHeaders.CHANNEL) Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) Long deliveryTag)
+            throws Exception {
+        log.info("message queued: ".concat(payload));
+        if (Integer.parseInt(payload) < 3) {
+            throw new Exception();
+        } else if (Integer.parseInt(payload) < 6) {
+            channel.basicReject(deliveryTag, false);
+        }
     }
     
 }
