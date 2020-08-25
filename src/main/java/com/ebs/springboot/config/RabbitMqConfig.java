86e79e7d080d8aa7c21dce1713eff77a5f2ccdb6
Esteban BS <estbs93@gmail.com>
2020-08-24 21:38:04 -0500

    Add properties to set a DLQ

--- src/main/java/com/ebs/springboot/config/RabbitMqConfig.java
+++ src/main/java/com/ebs/springboot/config/RabbitMqConfig.java
@@ -1,8 +1,11 @@
 package com.ebs.springboot.config;
 
+import java.util.HashMap;
+
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -13,8 +16,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMqConfig {
 
-	public static final String QUEUE_CREATED_MESSAGES = "ebs.messages.created";
 	public static final String EXCHANGE_CREATE_MESSAGES = "ebs.messages.create";
+	public static final String QUEUE_CREATED_MESSAGES = "ebs.messages.created";
+	
+	public static final String EXCHANGE_CREATE_MESSAGES_DLQ = "ebs.messages.create.dlq";
+	public static final String BINDING_CREATE_MESSAGES_DLQ = "ebs.messages.dlq";
+	public static final String QUEUE_CREATED_MESSAGES_DLQ = "ebs.messages.created.dlq";
+
+	private static final String DEAD_LETTER_EXCHANGE_ARGUMENT = "x-dead-letter-exchange";
+	private static final String DEAD_LETTER_ROUTINGKEY_ARGUMENT = "x-dead-letter-routing-key";
 
 	@Bean
 	public AmqpAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
@@ -27,9 +37,27 @@ public class RabbitMqConfig {
 	}
 
 	@Bean
-	Queue ebsMessagesQueue() {
-		return new Queue(QUEUE_CREATED_MESSAGES, true);
+	DirectExchange messagesDLExchange() {
+		return new DirectExchange(EXCHANGE_CREATE_MESSAGES_DLQ);
 	}
+
+	@Bean
+	Queue ebsMessagesDLQueue() {
+		return new Queue(QUEUE_CREATED_MESSAGES_DLQ, true);
+	}
+
+	@Bean
+	Queue ebsMessagesQueue() {
+		var arguments = new HashMap<String, Object>();
+		arguments.put(DEAD_LETTER_EXCHANGE_ARGUMENT, EXCHANGE_CREATE_MESSAGES_DLQ);
+		arguments.put(DEAD_LETTER_ROUTINGKEY_ARGUMENT, BINDING_CREATE_MESSAGES_DLQ);
+		return new Queue(QUEUE_CREATED_MESSAGES, true, false, false, arguments);
+	}
+
+	@Bean
+    Binding ebsMessagesDLQBinding() {
+        return BindingBuilder.bind(ebsMessagesDLQueue()).to(messagesDLExchange()).with(BINDING_CREATE_MESSAGES_DLQ);
+    }
 
 	@Bean
 	Binding messagesExchangeToEbsMessagesQueue() {
