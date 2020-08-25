c53de8f05a8b777b23c205ef75c458585fac8abb
Esteban BS <estbs93@gmail.com>
2020-08-25 13:34:46 -0500

    Create queues with DLQ

--- src/main/java/com/ebs/springboot/config/RabbitMqConfig.java
+++ src/main/java/com/ebs/springboot/config/RabbitMqConfig.java
@@ -17,7 +17,9 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMqConfig {
 
 	public static final String EXCHANGE_CREATE_MESSAGES = "ebs.messages.create";
-	public static final String QUEUE_CREATED_MESSAGES = "ebs.messages.created";
+	public static final String QUEUE_CREATED_MESSAGES_1 = "ebs.messages.created.1";
+	public static final String QUEUE_CREATED_MESSAGES_2 = "ebs.messages.created.2";
+	public static final String QUEUE_CREATED_MESSAGES_3 = "ebs.messages.created.3";
 	
 	public static final String EXCHANGE_CREATE_MESSAGES_DLQ = "ebs.messages.create.dlq";
 	public static final String BINDING_CREATE_MESSAGES_DLQ = "ebs.messages.dlq";
@@ -47,11 +49,27 @@ public class RabbitMqConfig {
 	}
 
 	@Bean
-	Queue ebsMessagesQueue() {
+	Queue ebsMessagesQueue1() {
 		var arguments = new HashMap<String, Object>();
 		arguments.put(DEAD_LETTER_EXCHANGE_ARGUMENT, EXCHANGE_CREATE_MESSAGES_DLQ);
 		arguments.put(DEAD_LETTER_ROUTINGKEY_ARGUMENT, BINDING_CREATE_MESSAGES_DLQ);
-		return new Queue(QUEUE_CREATED_MESSAGES, true, false, false, arguments);
+		return new Queue(QUEUE_CREATED_MESSAGES_1, true, false, false, arguments);
+	}
+
+	@Bean
+	Queue ebsMessagesQueue2() {
+		var arguments = new HashMap<String, Object>();
+		arguments.put(DEAD_LETTER_EXCHANGE_ARGUMENT, EXCHANGE_CREATE_MESSAGES_DLQ);
+		arguments.put(DEAD_LETTER_ROUTINGKEY_ARGUMENT, BINDING_CREATE_MESSAGES_DLQ);
+		return new Queue(QUEUE_CREATED_MESSAGES_2, true, false, false, arguments);
+	}
+
+	@Bean
+	Queue ebsMessagesQueue3() {
+		var arguments = new HashMap<String, Object>();
+		arguments.put(DEAD_LETTER_EXCHANGE_ARGUMENT, EXCHANGE_CREATE_MESSAGES_DLQ);
+		arguments.put(DEAD_LETTER_ROUTINGKEY_ARGUMENT, BINDING_CREATE_MESSAGES_DLQ);
+		return new Queue(QUEUE_CREATED_MESSAGES_3, true, false, false, arguments);
 	}
 
 	@Bean
@@ -60,8 +78,18 @@ public class RabbitMqConfig {
     }
 
 	@Bean
-	Binding messagesExchangeToEbsMessagesQueue() {
-		return BindingBuilder.bind(ebsMessagesQueue()).to(messagesExchange());
+	Binding messagesExchangeToEbsMessagesQueue1() {
+		return BindingBuilder.bind(ebsMessagesQueue1()).to(messagesExchange());
+	}
+
+	@Bean
+	Binding messagesExchangeToEbsMessagesQueue2() {
+		return BindingBuilder.bind(ebsMessagesQueue2()).to(messagesExchange());
+	}
+
+	@Bean
+	Binding messagesExchangeToEbsMessagesQueue3() {
+		return BindingBuilder.bind(ebsMessagesQueue3()).to(messagesExchange());
 	}
 
 }
