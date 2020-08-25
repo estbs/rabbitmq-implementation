152b785a42a179d43e82f2dc5e9c183ab55f21f7
Esteban BS <estbs93@gmail.com>
2020-08-24 21:40:13 -0500

    Use stringutils to queue a message

--- src/main/java/com/ebs/springboot/messages/MessagesProducerImpl.java
+++ src/main/java/com/ebs/springboot/messages/MessagesProducerImpl.java
@@ -1,20 +1,25 @@
 package com.ebs.springboot.messages;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
 @Component
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
 public class MessagesProducerImpl implements MessagesProducer {
 
     public static final String EXCHANGE_CREATE_MESSAGES = "ebs.messages.create";
     
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
+    RabbitTemplate rabbitTemplate;
 
     @Override
     public void queueMessage(int messageNumber) {
-        rabbitTemplate.convertAndSend(EXCHANGE_CREATE_MESSAGES, "", Integer.toString(messageNumber));
+        rabbitTemplate.convertAndSend(EXCHANGE_CREATE_MESSAGES, StringUtils.EMPTY, Integer.toString(messageNumber));
     }
     
 }
